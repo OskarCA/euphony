3fc51040eaf2356d25ed6fbb08d0a1f46f32fbad
Oskar Brännström <oskar@quad.se>
2018-03-03 18:46:23 +0100

    Add path to concert tickets

--- app/controllers/concert_tickets_controller.rb
+++ app/controllers/concert_tickets_controller.rb
@@ -1,5 +1,5 @@
 class ConcertTicketsController < ApplicationController
     def index
-      # redirect_to concert_tickets_path if user_signed_in?
+      redirect_to concert_tickets_path if user_signed_in?
     end
 end
